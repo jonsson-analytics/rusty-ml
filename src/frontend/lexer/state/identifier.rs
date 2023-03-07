22eab02ec4b21033159d400dc23f0fa9144e90a6
Nils Jonsson <nils.jonsson@jonsson-analytics.com>
2023-03-07 01:02:43 +0000

    Implemented identifiers.

--- src/frontend/lexer/state/identifier.rs
+++ src/frontend/lexer/state/identifier.rs
@@ -9,6 +9,15 @@ pub struct Identifier
   pub buffer: Vec<char>,
 }
 
+fn is_delimiting(char: char) -> bool
+{
+  match char {
+    | ' ' | '\t' | '\r' | '\n' | '(' | ')' | '{' | '}' | '[' | ']' | '`' =>
+      true,
+    | _ => false,
+  }
+}
+
 impl Feedable for Identifier
 {
   fn feed(
@@ -16,6 +25,21 @@ impl Feedable for Identifier
     char: Option<char>,
   ) -> FeedableResult
   {
-    todo!()
+    match char {
+      | None => FeedableResult::Finished {
+        state: State::empty(),
+        token: Token::identifier(String::from_iter(self.buffer.iter())),
+        consumed: false,
+      },
+      | Some(c) if is_delimiting(c) => FeedableResult::Finished {
+        state: State::empty(),
+        token: Token::identifier(String::from_iter(self.buffer.iter())),
+        consumed: false,
+      },
+      | Some(c) => {
+        self.buffer.push(c);
+        FeedableResult::Continue
+      },
+    }
   }
 }
