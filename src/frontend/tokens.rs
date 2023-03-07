3775e2432d8be4d243fe65424a0a4e08b9f9f348
Nils Jonsson <nils.jonsson@jonsson-analytics.com>
2023-03-07 13:32:17 +0000

    Implemented helper functions.
    Token::symbol and Token::keyword simplify the creation of tokens.

--- src/frontend/tokens.rs
+++ src/frontend/tokens.rs
@@ -1,9 +1,27 @@
 #[derive(Debug, Clone, PartialEq)]
-pub enum Token {
+pub enum Token
+{
   Symbol(String),
   Identifier,
   StringLiteral,
   Keyword(String),
   UnclosedComment,
   UnclosedString,
+}
+
+impl Token
+{
+  pub fn symbol<IntoString>(value: IntoString) -> Self
+  where
+    IntoString: Into<String>,
+  {
+    Self::Symbol(value.into())
+  }
+
+  pub fn keyword<IntoString>(value: IntoString) -> Self
+  where
+    IntoString: Into<String>,
+  {
+    Self::Keyword(value.into())
+  }
 }
