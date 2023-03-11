05d337fd016fe292f427dd20229ba1522ceb80c8
Nils Jonsson <nils.jonsson@jonsson-analytics.com>
2023-03-11 23:41:36 +0000

    Extended specification.

--- src/frontend/parser/backtracking_iterator.rs
+++ src/frontend/parser/backtracking_iterator.rs
@@ -62,3 +62,106 @@ where
     return result
   }
 }
+
+
+#[cfg(test)]
+mod spec
+{
+  use super::*;
+  use crate::frontend::lexer::Lexer;
+  use crate::frontend::parser::{
+    ExpectSyntax,
+    ParseError,
+    WithBacktracking,
+  };
+  use crate::frontend::tokens::Token;
+
+  #[test]
+  fn success_in_guarded_parse_consumes_tokens()
+  {
+    let mut lexer = Lexer::from_str("val foo = ;").with_backtracking();
+    assert_eq!(
+      lexer.breakpoint(|lexer| {
+        lexer.expect(Token::Keyword("val"))?;
+        lexer.expect(Token::Identifier)?;
+        lexer.expect(Token::Keyword("="))?;
+        Ok(())
+      }),
+      Ok(())
+    );
+    assert_eq!(lexer.expect(Token::Keyword(";")), Ok(Lexeme::keyword(";")));
+    assert_eq!(lexer.next(), None);
+  }
+
+  #[test]
+  fn failure_in_guarded_parse_does_not_consume_tokens()
+  {
+    let mut lexer = Lexer::from_str("val foo bar ;").with_backtracking();
+    let result = lexer.breakpoint(|lexer| {
+      lexer.expect(Token::Keyword("val"))?;
+      lexer.expect(Token::Identifier)?;
+      lexer.expect(Token::Keyword("="))?;
+      Ok(())
+    });
+    dbg!(lexer.cursor);
+    assert_eq!(
+      result,
+      Err(ParseError::UnexpectedToken {
+        expected: Token::Keyword("="),
+        actual: Lexeme::identifier("bar"),
+      })
+    );
+    assert_eq!(lexer.expect(Token::Keyword("val")), Ok(Lexeme::keyword("val")));
+    assert_eq!(lexer.expect(Token::Identifier), Ok(Lexeme::identifier("foo")));
+    assert_eq!(lexer.expect(Token::Identifier), Ok(Lexeme::identifier("bar")));
+    assert_eq!(lexer.expect(Token::Keyword(";")), Ok(Lexeme::keyword(";")));
+    assert_eq!(lexer.next(), None);
+  }
+
+  #[test]
+  fn success_in_nested_guarded_parse_consumes_tokens()
+  {
+    let mut lexer = Lexer::from_str("val foo = ;").with_backtracking();
+    assert_eq!(
+      lexer.breakpoint(|lexer| {
+        lexer.expect(Token::Keyword("val"))?;
+        lexer.breakpoint(|lexer| {
+          lexer.expect(Token::Identifier)?;
+          Ok(())
+        })?;
+        lexer.expect(Token::Keyword("="))?;
+        Ok(())
+      }),
+      Ok(())
+    );
+    assert_eq!(lexer.expect(Token::Keyword(";")), Ok(Lexeme::keyword(";")));
+    assert_eq!(lexer.next(), None);
+  }
+
+  #[test]
+  fn failure_in_nested_guarded_parse_does_not_consume_tokens()
+  {
+    let mut lexer = Lexer::from_str("val foo = ;").with_backtracking();
+    assert_eq!(
+      lexer.breakpoint(|lexer| {
+        lexer.expect(Token::Keyword("val"))?;
+        assert_eq!(
+          lexer.breakpoint(|lexer| {
+            lexer.expect(Token::Keyword("val"))?;
+            Ok(())
+          }),
+          Err(ParseError::UnexpectedToken {
+            expected: Token::Keyword("val"),
+            actual: Lexeme::identifier("foo")
+          })
+        );
+        lexer.expect(Token::Identifier)?;
+        lexer.expect(Token::Keyword("="))?;
+        Ok(())
+      }),
+      Ok(())
+    );
+    assert_eq!(lexer.expect(Token::Keyword(";")), Ok(Lexeme::keyword(";")));
+    assert_eq!(lexer.next(), None);
+  }
+}
